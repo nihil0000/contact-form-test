6e47a33e70d343bcff3de9440297c81dbb8dbb10
nihil0000 <nihil.dev0@gmail.com>
2025-02-11 21:20:11 +0900

    feat: Add authentication routes and views

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// register page
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+// register
+Route::post('register', [AuthController::class, 'register']);
+
+// login page
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// login
+Route::post('/login', [AuthController::class, 'login']);
+
+/*
+admin page
+未ログイン状態の場合、loginページに遷移する
+*/
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware('auth')->name('admin');
+
+// logout
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+// TODO: controllerを使用してviewを返す
+Route::get('/confirm', function () {
+    return view('/confirm');
+})->name('confirm');
+
+// TODO: controllerを使用してviewを返す
+Route::get('/thanks', function () {
+    return view('/thanks');
+})->name('thanks');
+
+
+Route::get('/', [ContactController::class, 'index']);
