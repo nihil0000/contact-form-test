21bf09ccfd5bf5d055ea725ae8c133d51e5f1e7e
nihil0000 <nihil.dev0@gmail.com>
2025-02-11 22:33:48 +0900

    fix: Correct typo in LoginRequest import

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
-use App\Http\Requests\LoginrRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
